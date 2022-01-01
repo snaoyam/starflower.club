aa69d506643cce689872427669a048b394e8ffe1
star <snaoyam@gmail.com>
2022-01-01 10:34:24 +0900

    [Fix] changed route url

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,6 @@ const {getPublic} = require("./middleware/user")
 const validateToken = require("./middleware/validateToken")
 //const userRoutes = require('./routes/user')
 
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -19,18 +18,18 @@ var corsOptions = {
 };
 app.use(cors(corsOptions))
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ message: "Backend for starflower.club/member" })
 })
 
-app.post("/register", register)
-app.post("/login", login)
-app.use("/logout", logout)
+app.post("/api/register", register)
+app.post("/api/login", login)
+app.use("/api/logout", logout)
 
 /*need validation*/
 app.use(validateToken)
 
-app.post("/public", getPublic)
+app.post("/api/public", getPublic)
 
 const PORT = Number(process.env.PORT) || 8080
 app.listen(PORT, () => {
